a2dd6aaeda547013d9b80d147d348c1f786e3c9f
MartinToruan <martinkristopel@hotmail.com>
2019-03-19 18:39:55 +0700

    [blog] Update filter to use bson empty value

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -158,7 +158,7 @@ func (*server) DeleteBlog(ctx context.Context, req *blogpb.DeleteBlogRequest) (*
 func (*server) ListBlog(req *blogpb.ListBlogRequest, stream blogpb.BlogService_ListBlogServer) error{
 	fmt.Println("List Blog Request")
 
-	cur, err := collection.Find(context.Background(), nil)
+	cur, err := collection.Find(context.Background(), bson.D{})
 	if err != nil{
 		return status.Errorf(
 			codes.Internal,
